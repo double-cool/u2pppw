4ac8099c181ae6382c83e020ef2816b5fb3a7a78
double-cool <2226828864@qq.com>
2022-07-11 23:52:54 +0800

    fix 正则 匹配

--- carwler/main.go
+++ carwler/main.go
@@ -1,14 +1,14 @@
-package main
-
-import (
-	"u2pppw/carwler/engine"
-	"u2pppw/carwler/zhenai/parser"
-)
-
-func main() {
-	engine.Run(engine.Request{
-		Url:        "http://www.zhenai.com/zhenghun",
-		ParserFunc: parser.ParseCityList,
-	})
-
-}
+////package main
+//
+//import (
+//	"u2pppw/carwler/engine"
+//	"u2pppw/carwler/zhenai/parser"
+//)
+//
+//func main() {
+//	engine.Run(engine.Request{
+//		Url:        "http://www.zhenai.com/zhenghun",
+//		ParserFunc: parser.ParseCityList,
+//	})
+//
+//}
